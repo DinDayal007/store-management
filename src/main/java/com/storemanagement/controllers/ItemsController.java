f2186910efa89f62e637c051116045443f4cdecb
mohyehia <mohammedyehia99@gmail.com>
2017-09-22 16:03:04 +0200

    Finish items retrieval

--- src/main/java/com/storemanagement/controllers/ItemsController.java
+++ src/main/java/com/storemanagement/controllers/ItemsController.java
@@ -1,17 +1,21 @@
 package com.storemanagement.controllers;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.storemanagement.entities.Item;
+import com.storemanagement.services.EntityService;
 @WebServlet("/items")
 public class ItemsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		List<Item> items = EntityService.getAllObjects(Item.class);
+		request.setAttribute("items", items);
 		request.getRequestDispatcher("items/index.jsp").forward(request, response);
 	}
 
